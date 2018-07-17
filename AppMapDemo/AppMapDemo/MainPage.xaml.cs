4c45f6804eb7c327485b61367931050b6399f91e
Hugo G Fernandez R <hugh_fernandez@hotmail.com>
2018-07-17 00:28:21 -0500

    First implementation

---
+++ AppMapDemo/AppMapDemo/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace AppMapDemo
+{
+	public partial class MainPage : ContentPage
+	{
+        // Dictionary that associates names with basemaps
+        private readonly Dictionary<string, Basemap> basemapOptions = new Dictionary<string, Basemap>()
+        {
+            {"Streets (Raster)", Basemap.CreateStreets()},
+            {"Streets (Vector)", Basemap.CreateStreetsVector()},
+            {"Streets - Night (Vector)", Basemap.CreateStreetsNightVector()},
+            {"Imagery (Raster)", Basemap.CreateImagery()},
+            {"Imagery with Labels (Raster)", Basemap.CreateImageryWithLabels()},
+            {"Imagery with Labels (Vector)", Basemap.CreateImageryWithLabelsVector()},
+            {"Dark Gray Canvas (Vector)", Basemap.CreateDarkGrayCanvasVector()},
+            {"Light Gray Canvas (Raster)", Basemap.CreateLightGrayCanvas()},
+            {"Light Gray Canvas (Vector)", Basemap.CreateLightGrayCanvasVector()},
+            {"Navigation (Vector)", Basemap.CreateNavigationVector()},
+            {"OpenStreetMap (Raster)", Basemap.CreateOpenStreetMap()}
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Title = "Mostrar coordenadas";
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Create a new basemap using the streets base layer
+            Basemap myBasemap = Basemap.CreateStreets();
+
+            // Create a new map based on the streets basemap
+            Map myMap = new Map(myBasemap);
+
+
+            // Assign the map to the MapView
+            MyMapView.Map = myMap;
+
+            // Wire up the MapView GeoVewTapped event
+            MyMapView.GeoViewTapped += MyMapView_GeoViewTapped;
+        }
+
+        private void MyMapView_GeoViewTapped(object sender, Esri.ArcGISRuntime.Xamarin.Forms.GeoViewInputEventArgs e)
+        {
+            // Get the user-tapped location
+            MapPoint mapLocation = e.Location;
+
+            // Project the user-tapped map point location to a geometry
+            Geometry myGeometry = GeometryEngine.Project(mapLocation, SpatialReferences.Wgs84);
+
+            // Convert to geometry to a traditional Lat/Long map point
+            MapPoint projectedLocation = (MapPoint)myGeometry;
+
+            // Format the display callout string based upon the projected map point (example: "Lat: 100.123, Long: 100.234")
+            string mapLocationDescription = string.Format("Lat: {0:F3} Long:{1:F3}", projectedLocation.Y, projectedLocation.X);
+
+            // Create a new callout definition using the formatted string
+            CalloutDefinition myCalloutDefinition = new CalloutDefinition("Location:", mapLocationDescription);
+
+            // Display the callout
+            MyMapView.ShowCalloutAt(mapLocation, myCalloutDefinition);
+        }
+
+
+        private async void OnChangeBasemapButtonClicked(object sender, EventArgs e)
+        {
+            // Show sheet and get title from the selection
+            var selectedBasemap =
+                await DisplayActionSheet("Select basemap", "Cancel", null, basemapOptions.Keys.ToArray());
+
+            if (selectedBasemap != "Cancel")
+            {
+                // Retrieve the basemap from the dictionary
+                MyMapView.Map.Basemap = basemapOptions[selectedBasemap];
+            }
+        }
+    }
+}
